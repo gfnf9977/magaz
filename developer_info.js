e6b4645a1d36324b5711f7fe648ad3efddc51aa6
gfnf9977 <145157618+gfnf9977@users.noreply.github.com>
2025-06-04 13:36:56 +0300

    lab7adding

--- developer_info.js
+++
@@ -1,6 +0,0 @@
-function showDeveloperInfo(lastName = "Шевченко", firstName = "Олександр", position = "Розробник") {
-    alert("Інформація про розробника:\n\n" +
-          "Прізвище: " + lastName + "\n" +
-          "Ім'я: " + firstName + "\n" +
-          "Посада: " + position);
-}
